a0f08a140d3e6c2468015e13ddb4e3a476c00688
Victor <victor.vasilev1991@gmail.com>
2017-01-01 18:00:25 +0300

    added setUp(self) and tearDown(self) methods

--- sum_func.py
+++ sum_func.py
@@ -3,9 +3,11 @@ from selenium import webdriver
 
 class GeneralFuncTests(unittest.TestCase):
 
-    def test_JSCALC009(self):
+    def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.get('http://test.job.klika-tech.com.s3-website.eu-central-1.amazonaws.com/')
+
+    def test_JSCALC009(self):
         self.driver.find_element_by_id('btn-2').click()
         self.driver.find_element_by_xpath('//div[@class="operators"]/div').click()
         self.driver.find_element_by_id('btn-3').click()
@@ -13,8 +15,6 @@ class GeneralFuncTests(unittest.TestCase):
         assert '5' in self.driver.find_element_by_id('input').text
 
     def test_JSCALC010(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get('http://test.job.klika-tech.com.s3-website.eu-central-1.amazonaws.com/')
         self.driver.find_element_by_id('btn-2').click()
         self.driver.find_element_by_xpath('//div[@class="operators"]/div').click()
         self.driver.find_element_by_id('btn-6').click()
@@ -26,8 +26,6 @@ class GeneralFuncTests(unittest.TestCase):
         assert '70.97' in self.driver.find_element_by_id('input').text
 
     def test_JSCALC011(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get('http://test.job.klika-tech.com.s3-website.eu-central-1.amazonaws.com/')
         self.driver.find_element_by_id('btn-1').click()
         self.driver.find_element_by_class_name('btn-4').click()
         self.driver.find_element_by_xpath('//div[@class="numbers"][4]/div[2]').click()
@@ -43,8 +41,6 @@ class GeneralFuncTests(unittest.TestCase):
         assert '30' in self.driver.find_element_by_id('input').text
 
     def test_JSCALC012(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get('http://test.job.klika-tech.com.s3-website.eu-central-1.amazonaws.com/')
         self.driver.find_element_by_class_name('btn-5').click()
         self.driver.find_element_by_id('btn-6').click()
         self.driver.find_element_by_xpath('//div[@class="numbers"][4]/div[2]').click()
@@ -57,8 +53,6 @@ class GeneralFuncTests(unittest.TestCase):
         assert '64.2' in self.driver.find_element_by_id('input').text
 
     def test_JSCALC013(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get('http://test.job.klika-tech.com.s3-website.eu-central-1.amazonaws.com/')
         self.driver.find_element_by_id('btn-1').click()
         self.driver.find_element_by_xpath('//div[@class="numbers"]/div').click()
         self.driver.find_element_by_xpath('//div[@class="operators"]/div').click()
@@ -67,16 +61,12 @@ class GeneralFuncTests(unittest.TestCase):
         assert '17' in self.driver.find_element_by_id('input').text
 
     def test_JSCALC014(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get('http://test.job.klika-tech.com.s3-website.eu-central-1.amazonaws.com/')
         self.driver.find_element_by_id('btn-9').click()
         self.driver.find_element_by_xpath('//div[@class="operators"]/div').click()
         self.driver.find_element_by_id('result').click()
         assert '9' in self.driver.find_element_by_id('input').text
 
     def test_JSCALC015(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get('http://test.job.klika-tech.com.s3-website.eu-central-1.amazonaws.com/')
         self.driver.find_element_by_id('btn-2').click()
         self.driver.find_element_by_xpath('//div[@class="operators"]/div').click()
         self.driver.find_element_by_id('btn-3').click()
@@ -89,8 +79,6 @@ class GeneralFuncTests(unittest.TestCase):
         assert '17.5' in self.driver.find_element_by_id('input').text
 
     def test_JSCALC016(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get('http://test.job.klika-tech.com.s3-website.eu-central-1.amazonaws.com/')
         self.driver.find_element_by_id('btn-2').click()
         self.driver.find_element_by_id('btn-2').click()
         self.driver.find_element_by_id('btn-2').click()
@@ -128,4 +116,7 @@ class GeneralFuncTests(unittest.TestCase):
         self.driver.find_element_by_id('btn-2').click()
         self.driver.find_element_by_id('result').click()
         assert '44444444444444444' in self.driver.find_element_by_id('input').text
+
+    def tearDown(self):
+        self.driver.quit()
 
